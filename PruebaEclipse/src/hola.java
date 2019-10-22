18a13f2cb9e191d900293c4b8c1edc6edc9c9110
SoniaCalvo <scalvob@viewnext.com>
2019-10-22 18:48:32 +0200

    commit1

---
+++ PruebaEclipse/src/hola.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author 0012591
+ *
+ */
+public class hola {
+
+	/**
+	 * 
+	 */
+	public hola() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hola");
+
+	}
+
+}
