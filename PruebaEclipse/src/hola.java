49f378de5e388c4912f77b02d0d47353fa1bc463
SoniaCalvo <scalvob@viewnext.com>
2019-10-22 18:49:33 +0200

    commit2

--- PruebaEclipse/src/hola.java
+++ PruebaEclipse/src/hola.java
@@ -20,7 +20,7 @@ public class hola {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hola");
+        System.out.println("holaaa");
 
 	}
 
